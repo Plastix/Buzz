c9de158c4155ab0f75f250fb82f77c5eb17f73c9
Aidan Pieper <apieper@strava.com>
2021-03-21 13:29:34 -0700

    Hexagonal buttons + grid

--- app/src/main/java/io/github/plastix/buzz/detail/PuzzleDetailUi.kt
+++ app/src/main/java/io/github/plastix/buzz/detail/PuzzleDetailUi.kt
@@ -1,15 +1,24 @@
 package io.github.plastix.buzz.detail
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.AbsoluteCutCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Autorenew
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Outline
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Density
+import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import io.github.plastix.buzz.Puzzle
+import kotlin.math.cos
+import kotlin.math.sin
 
 @Composable
 fun PuzzleDetailUi(viewState: PuzzleDetailViewState) {
@@ -37,34 +46,89 @@ fun PuzzleBoard(response: Puzzle) {
 
 @Composable
 fun PuzzleKeypad(centerLetter: Char, outterLetters: List<Char>) {
-    Box {
-        Column(horizontalAlignment = Alignment.CenterHorizontally) {
-            Row {
-                KeypadButton(outterLetters.getOrElse(0) { ' ' })
-                KeypadButton(outterLetters.getOrElse(1) { ' ' })
-                KeypadButton(outterLetters.getOrElse(2) { ' ' })
-            }
-            Spacer(Modifier.size(4.dp))
+    Layout(
+        content = {
             KeypadButton(centerLetter)
-            Spacer(Modifier.size(4.dp))
-            Row {
-                KeypadButton(outterLetters.getOrElse(3) { ' ' })
-                KeypadButton(outterLetters.getOrElse(4) { ' ' })
-                KeypadButton(outterLetters.getOrElse(5) { ' ' })
+            KeypadButton(outterLetters.getOrElse(0) { ' ' })
+            KeypadButton(outterLetters.getOrElse(1) { ' ' })
+            KeypadButton(outterLetters.getOrElse(2) { ' ' })
+            KeypadButton(outterLetters.getOrElse(3) { ' ' })
+            KeypadButton(outterLetters.getOrElse(4) { ' ' })
+            KeypadButton(outterLetters.getOrElse(5) { ' ' })
+        },
+        modifier = Modifier
+    ) { measurables, constraints ->
+        val placeables = measurables.map { measurable ->
+            measurable.measure(constraints)
+        }
+        val spacing = 4.dp.roundToPx()
+        val buttonHeight = placeables.first().height
+        val totalHeight: Int = (buttonHeight * 3) + (spacing * 2)
+
+        layout(totalHeight, totalHeight) {
+            // Place center button
+            val viewCx = totalHeight / 2
+            val viewCy = totalHeight / 2
+            placeables.first().place(viewCx, viewCy)
+            // Place surrounding buttons
+            placeables.drop(1).forEachIndexed { index, placeable ->
+                val offset = buttonHeight + spacing
+                val angle = 30 + (index * 60)
+                val x = viewCx + cos(angle * (Math.PI / 180)) * offset
+                val y = viewCy + sin(angle * (Math.PI / 180)) * offset
+                placeable.place(x.toInt(), y.toInt())
             }
         }
     }
 }
 
 @Composable
+@Preview
+fun PreviewPuzzleKeypad() {
+    PuzzleKeypad(centerLetter = 'x', outterLetters = listOf('a', 'b', 'c', 'd', 'e', 'f'))
+}
+
+
+@Composable
 fun KeypadButton(letter: Char) {
     Button(
-        modifier = Modifier.size(64.dp, 32.dp),
-        shape = AbsoluteCutCornerShape(16.dp),
+        modifier = Modifier.size(64.dp),
+        shape = RegularHexagonalShape(),
         onClick = {}
     ) {
         Text(letter.toString())
     }
+}
+
+class RegularHexagonalShape : Shape {
+    override fun createOutline(
+        size: Size,
+        layoutDirection: LayoutDirection,
+        density: Density
+    ): Outline {
+        val path = Path()
+        val cx = size.width / 2
+        val cy = size.height / 2
+        var angle = 0
+        for (i in 0 until 6) {
+            angle += 60
+            val x = cos(angle * (Math.PI / 180)) * cx + cx
+            val y = sin(angle * (Math.PI / 180)) * cy + cy
+            if (i == 0) {
+                path.moveTo(x.toFloat(), y.toFloat())
+            } else {
+                path.lineTo(x.toFloat(), y.toFloat())
+            }
+        }
+        return Outline.Generic(path)
+    }
+}
+
+
+@Composable
+@Preview
+fun PreviewKeypadButton() {
+    KeypadButton(letter = 'x')
 }
 
 @Composable

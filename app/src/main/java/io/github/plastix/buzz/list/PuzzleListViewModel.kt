8f065e3b19b19c027e39175c90317d52796c31ab
JEphron <jephron63@gmail.com>
2021-03-21 17:53:55 -0700

    Human-ish date

--- app/src/main/java/io/github/plastix/buzz/list/PuzzleListViewModel.kt
+++ app/src/main/java/io/github/plastix/buzz/list/PuzzleListViewModel.kt
@@ -6,6 +6,18 @@ import io.github.plastix.buzz.network.PuzzleFetcher
 import io.github.plastix.buzz.Result
 import io.github.plastix.buzz.persistence.PuzzleRepository
 import kotlinx.coroutines.launch
+import java.text.ParseException
+import java.text.SimpleDateFormat
+
+fun reformatDate(date: String, fromFmt: String, toFmt: String): String {
+    val fromFormat = SimpleDateFormat(fromFmt)
+    val toFormat = SimpleDateFormat(toFmt)
+    return try {
+        toFormat.format(fromFormat.parse(date)!!)
+    } catch (e: ParseException) {
+        date
+    }
+}
 
 class PuzzleListViewModel(
     private val fetcher: PuzzleFetcher,
@@ -31,7 +43,7 @@ class PuzzleListViewModel(
     private fun Puzzle.toRowState(): PuzzleRowState {
         return PuzzleRowState(
             puzzleId = date,
-            displayString = date
+            displayString = reformatDate(date, "yyyy-MM-dd", "EEEE MMMM d, yyyy")
         )
     }
 

12a3e9b0958fae9bd1de868689218808050c5ab3
Aidan Pieper <apieper@strava.com>
2021-04-09 08:03:32 -0700

    First pass at blocking bad words from dictionary

--- dataset/src/main/java/io/github/plastix/buzz/dataset/DatasetGenerator.kt
+++ dataset/src/main/java/io/github/plastix/buzz/dataset/DatasetGenerator.kt
@@ -8,46 +8,76 @@ import io.github.plastix.buzz.core.toCharacterSet
 import io.plastix.github.buzz.dataset.Database
 import java.io.File
 import java.nio.file.Paths
+import java.util.concurrent.TimeUnit
+import kotlin.system.measureTimeMillis
+import kotlin.time.measureTime
 
 
 const val DB_EXPORT_FOLDER = "app/src/main/assets/database/"
 const val DB_EXPORT_NAME = "dictionary.db"
+const val RESOURCES_PATH = "dataset/src/main/resources/"
 
 /**
  * Preprocesses a Dictionary of words into a SQLite database which is bundled with the main
  * Android app.
  */
 fun main() {
-    // Delete the existing table file
-    File("$DB_EXPORT_FOLDER$DB_EXPORT_NAME").delete()
+    var totalWords = 0
+    var procesedCount = 0
+    val profaneWords = mutableSetOf<String>()
+    var puzzleSeeds = 0
+    val durationMs = measureTimeMillis {
+        // Delete the existing table file
+        File("$DB_EXPORT_FOLDER$DB_EXPORT_NAME").delete()
 
-    val driver: SqlDriver = JdbcSqliteDriver("jdbc:sqlite:$DB_EXPORT_FOLDER$DB_EXPORT_NAME")
-    Database.Schema.create(driver)
-    val database = Database(driver)
-    val queries = database.schemaQueries
+        val driver: SqlDriver = JdbcSqliteDriver("jdbc:sqlite:$DB_EXPORT_FOLDER$DB_EXPORT_NAME")
+        Database.Schema.create(driver)
+        val database = Database(driver)
+        val queries = database.schemaQueries
 
-    val wordPath =
-        Paths.get("dataset/src/main/java/io/github/plastix/buzz/dataset/words").toAbsolutePath()
-            .toString()
-    val reader = File(wordPath).bufferedReader()
-    reader.lines().forEach { word ->
-        if (word.isValid()) {
-            val charSet = word.toCharacterSet()
-            queries.addWord(charSet.toLong(), word)
+        val badWords = Paths.get("${RESOURCES_PATH}bad_words").toAbsolutePath().toString()
+        val denyList: Set<String> = File(badWords).readLines().toSet()
 
-            if (charSet.size == Constants.LETTER_COUNT) {
-                queries.addPuzzleSeed(charSet.toLong())
+        val dictionaryPath = Paths.get("${RESOURCES_PATH}words").toAbsolutePath().toString()
+        val reader = File(dictionaryPath).bufferedReader()
+        reader.lines().forEach { word ->
+            if (word.isValid()) {
+                if (word !in denyList) {
+                    val charSet = word.toCharacterSet()
+                    queries.addWord(charSet.toLong(), word)
+
+                    if (charSet.size == Constants.LETTER_COUNT) {
+                        queries.addPuzzleSeed(charSet.toLong())
+                        puzzleSeeds++
+                    }
+                    procesedCount++
+
+                } else {
+                    profaneWords.add(word)
+                }
             }
+            totalWords++
         }
+
     }
 
+    val seconds = TimeUnit.MILLISECONDS.toSeconds(durationMs)
+    println(
+        """
+        Processed $totalWords words from word list in $seconds seconds!
+        Final count: $procesedCount
+        Blocked profane words: ${profaneWords.size}
+
+        Found $puzzleSeeds possible puzzle seeds
+    """.trimIndent()
+    )
 }
 
 /**
  * Words are only valid answers if are at least 4 letters and contain no special characters.
  * Spaces or capital checking also removes proper nouns from our dictionary list
  */
-fun String.isValid(): Boolean {
+private fun String.isValid(): Boolean {
     return length >= 4 && none {
         it < 'a' || it > 'z'
     }

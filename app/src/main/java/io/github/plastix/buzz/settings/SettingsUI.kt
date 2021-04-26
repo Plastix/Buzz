f30ca09111ea91546a1b91199a7a9e14bebced8f
Aidan Pieper <apieper@strava.com>
2021-04-26 07:47:28 -0700

    Add logo to info dialog

--- app/src/main/java/io/github/plastix/buzz/settings/SettingsUI.kt
+++ app/src/main/java/io/github/plastix/buzz/settings/SettingsUI.kt
@@ -3,6 +3,7 @@ package io.github.plastix.buzz.settings
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
@@ -13,6 +14,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -53,38 +55,57 @@ fun SettingsUi(
             }
         )
         if (showInfoDialog.value) {
-            CustomDialog(
+            InfoDialog(
                 onDismiss = { showInfoDialog.value = false },
-                title = stringResource(R.string.about_dialog_title)
-            ) {
-                Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                    Text(
-                        text = stringResource(R.string.about_dialog_body),
-                        textAlign = TextAlign.Center
-                    )
-                    Spacer(Modifier.height(16.dp))
-                    val clickCounter = remember { mutableStateOf(0) }
-                    Text(text = "v ${BuildConfig.VERSION_NAME}",
-                        modifier = Modifier.noRippleClickable {
-                            if (clickCounter.value >= 4) {
-                                devMenuToggled.invoke()
-                                clickCounter.value = 0
-                            } else {
-                                clickCounter.value++
-                            }
-                        }
-                    )
-                    Text("(${BuildConfig.VERSION_CODE})")
-                    Spacer(Modifier.height(16.dp))
-                    OutlinedButton(
-                        onClick = onGiveFeedback,
-                        colors = ButtonDefaults.outlinedButtonColors(
-                            contentColor = MaterialTheme.colors.onSurface,
-                        )
-                    ) {
-                        Text(stringResource(R.string.about_dialog_give_feedback))
+                onGiveFeedback = onGiveFeedback,
+                devMenuToggled = devMenuToggled
+            )
+        }
+    }
+}
+
+@Composable
+fun InfoDialog(
+    onDismiss: () -> Unit,
+    onGiveFeedback: () -> Unit,
+    devMenuToggled: () -> Unit
+) {
+    CustomDialog(
+        onDismiss = onDismiss,
+        title = stringResource(R.string.about_dialog_title)
+    ) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Text(
+                text = stringResource(R.string.about_dialog_body),
+                textAlign = TextAlign.Center
+            )
+            Spacer(Modifier.height(16.dp))
+            Icon(
+                modifier = Modifier.size(50.dp),
+                painter = painterResource(id = R.drawable.ic_bee_black),
+                contentDescription = stringResource(R.string.puzzle_list_empty_icon),
+            )
+            Spacer(Modifier.height(16.dp))
+            val clickCounter = remember { mutableStateOf(0) }
+            Text(text = "v ${BuildConfig.VERSION_NAME}",
+                modifier = Modifier.noRippleClickable {
+                    if (clickCounter.value >= 4) {
+                        devMenuToggled.invoke()
+                        clickCounter.value = 0
+                    } else {
+                        clickCounter.value++
                     }
                 }
+            )
+            Text("(${BuildConfig.VERSION_CODE})")
+            Spacer(Modifier.height(16.dp))
+            OutlinedButton(
+                onClick = onGiveFeedback,
+                colors = ButtonDefaults.outlinedButtonColors(
+                    contentColor = MaterialTheme.colors.onSurface,
+                )
+            ) {
+                Text(stringResource(R.string.about_dialog_give_feedback))
             }
         }
     }

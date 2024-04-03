2270f641128f63a69c3b0e2234a6ed3390e69f79
Aidan Pieper <aidanpieper@gmail.com>
2024-04-03 08:13:25 -0700

    Fixed Compose snackbar to use LaunchedEffect

--- app/src/main/java/io/github/plastix/buzz/list/PuzzleListUi.kt
+++ app/src/main/java/io/github/plastix/buzz/list/PuzzleListUi.kt
@@ -2,19 +2,53 @@ package io.github.plastix.buzz.list
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.defaultMinSize
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.*
+import androidx.compose.material.AlertDialog
+import androidx.compose.material.Card
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.ScaffoldState
+import androidx.compose.material.Snackbar
+import androidx.compose.material.SnackbarDuration
+import androidx.compose.material.SnackbarHost
+import androidx.compose.material.SnackbarResult
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.TextButton
+import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.*
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Casino
+import androidx.compose.material.icons.filled.DeleteForever
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.filled.Verified
+import androidx.compose.material.primarySurface
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.compositionLocalOf
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color.Companion.White
@@ -33,7 +67,6 @@ import io.github.plastix.buzz.PuzzleType
 import io.github.plastix.buzz.R
 import io.github.plastix.buzz.theme.BuzzTheme
 import io.github.plastix.buzz.util.SwipeDismiss
-import kotlinx.coroutines.launch
 
 val LocalScaffoldState = compositionLocalOf<ScaffoldState> { error("No scaffold state provided") }
 
@@ -132,10 +165,9 @@ fun UndoDeleteSnackbar(
     activeSnackbar: Snackbar.UndoPuzzleDeletion
 ) {
     val scaffoldState: ScaffoldState = LocalScaffoldState.current
-    val scope = rememberCoroutineScope()
     val text = stringResource(R.string.puzzle_list_undo_snackbar_description)
     val action = stringResource(R.string.undo)
-    scope.launch {
+    LaunchedEffect(key1 = "undo_snackbar") {
         val result = scaffoldState.snackbarHostState.showSnackbar(
             message = text,
             actionLabel = action,
@@ -269,6 +301,7 @@ fun PuzzleRow(puzzleRow: PuzzleRowState) {
                     PuzzleType.GENERATED -> Icons.Default.Casino to stringResource(
                         R.string.puzzle_generated_content_description
                     )
+
                     PuzzleType.DOWNLOADED -> Icons.Default.Verified to stringResource(
                         R.string.puzzle_downloaded_content_description
                     )
